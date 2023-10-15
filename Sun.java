4704cbb8eabc7fe5bf762619ed021a2610291656
Charles Dobson <79115124+DobbaWon@users.noreply.github.com>
2023-10-15 18:41:37 +0100

    Created a Sun class
    Created a Sun class to make creating a sun easier.

---
+++ Sun.java
@@ -0,0 +1,11 @@
+// This is a basic class that fills in details into the CelestialBody class to display the Sun:
+public class Sun extends CelestialBody{
+    public Sun(){
+        /* Parameters:
+         * Diameter = 100
+         * Colour = Yellow
+         * Distance = 0
+         */
+        super(100, "YELLOW", 0);      
+    }
+}
